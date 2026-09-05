11e048883dc1efd12df890e2767fd6f19c5d193e
Max Heinritz <maxheinritz@gmail.com>
2026-09-05 16:17:50 -0500

    add startup script

---
+++ scripts/set_up_all_nodes.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# usage: ./scripts/setup_all.sh <netid>
+NETID=$1
+REPO=git@github.com:maxh/cmgrep.git
+
+for i in 01 02 03 04 05 06 07 08 09 10; do
+    host=fa26-cs425-72$i.cs.illinois.edu
+    ssh -A $NETID@$host "
+        git clone $REPO 2>/dev/null || (cd cmgrep && git pull)
+        cd cmgrep
+        go build -o cmgrep .
+        ./scripts/gen_logs.py
+        pkill -x cmgrep
+        nohup ./cmgrep serve > server.out 2>&1 < /dev/null &
+    "
+done
