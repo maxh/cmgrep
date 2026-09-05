6db8395678301707524530f2411e3c8bd887ebde
Max Heinritz <maxheinritz@gmail.com>
2026-09-05 16:21:24 -0500

    use http

--- scripts/set_up_all_nodes.sh
+++ scripts/set_up_all_nodes.sh
@@ -2,11 +2,11 @@
 
 # usage: ./scripts/setup_all.sh <netid>
 NETID=$1
-REPO=git@github.com:maxh/cmgrep.git
+REPO=https://github.com/maxh/cmgrep.git
 
 for i in 01 02 03 04 05 06 07 08 09 10; do
     host=fa26-cs425-72$i.cs.illinois.edu
-    ssh -A $NETID@$host "
+    ssh $NETID@$host "
         git clone $REPO 2>/dev/null || (cd cmgrep && git pull)
         cd cmgrep
         go build -o cmgrep .
